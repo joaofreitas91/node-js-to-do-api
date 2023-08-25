7888e5a806c5e9418e6836551820b62f4753003e
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-08-25 17:21:26 -0300

    feat: list and create task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import { createServer } from 'http'
+import { randomUUID } from 'crypto'
+
+const tasks = []
 
 const server = createServer((req, res) => {
   const { method, url } = req
 
-  console.log({ method, url })
+  if (method === 'POST' && url === '/tasks') {
+    tasks.push({
+      id: randomUUID(),
+      title: 'Make a sandwich',
+      description: 'I need eat a delicious sandwich',
+      completed_at: null,
+      created_at: new Date().toISOString(),
+      updated_at: null,
+    })
 
-  res.end('Hello World!')
+    res.end()
+    return
+  }
+
+  if (method === 'GET' && url === '/tasks') {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(tasks))
+
+    return
+  }
+
+  res.writeHead(404).end(JSON.stringify({ error: 'not found' }))
 })
 
 const port = 3333
